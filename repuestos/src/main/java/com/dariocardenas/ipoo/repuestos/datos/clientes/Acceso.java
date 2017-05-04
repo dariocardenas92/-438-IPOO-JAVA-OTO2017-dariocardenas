cf8dda2f1b0158a4e6cfda7b56e12c0eaf5708e5
alumno-202 <alumno-202@M202-19.uaa.local>
2017-05-03 21:44:55 -0400

    Agregada clase de Acceso

---
+++ repuestos/src/main/java/com/dariocardenas/ipoo/repuestos/datos/clientes/Acceso.java
@@ -0,0 +1,16 @@
+package com.dariocardenas.ipoo.repuestos.datos.clientes;
+
+import java.util.Date;
+
+public class Acceso {
+
+	private Date fechaHoraInicio;
+	private Date fechaHoraFin;
+	private String ip;
+	private Clientes cliente;
+	
+	public Acceso(){
+		
+	}
+	
+}
